d4df7d2c7a3201323dbf52b5a6225d61c90722ac
mchlup <132652169+mchlup@users.noreply.github.com>
2026-08-29 09:44:26 +0200

    Delete SanityChecks.h

--- SanityChecks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-namespace SanityChecks {
-  // Runs quick non-blocking checks and prints warnings to Serial.
-  // Safe to call even if some subsystems are disabled.
-  void runOnce();
-}
